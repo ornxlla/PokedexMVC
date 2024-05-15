68b464b1dd3216a64f042d0cf4554d3928802063
Ornella Alonso Reyes <ornxalonso@gmail.com>
2024-05-14 21:19:38 -0300

    holi..

---
+++ model/UserModel.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace model;
+
+class UserModel
+{
+ private $database;
+
+    public function __construct($database)
+    {
+        $this->database = $database;
+    }
+
+    public function getUsuario()
+    {
+        return $this->database->query("SELECT * FROM login");
+    }
+
+}
